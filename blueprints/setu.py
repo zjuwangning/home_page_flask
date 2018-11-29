b7f08a0352b50f917a80f360999e95808071d225
ReidWang <zjuwangning@zju.edu.cn>
2018-11-29 18:48:14 +0800

    1.os模块部分使用,文件/目录等 2.目录获取, 图片浏览, 文件上传

--- blueprints/setu.py
+++ blueprints/setu.py
@@ -7,29 +7,28 @@ from decorator import login_required
 SET_PATH = "E:\picture"
 set_bp = Blueprint('set', __name__, url_prefix='/contents')
 
+path_dict = {}
+for root, path, file in os.walk(SET_PATH):
+    for name in file:
+        path_dict[name] = os.path.join(root, name)
+sub_path_dict = {}
+
 
 @set_bp.route('/',  methods=['GET', 'POST'])
 @login_required
 def contents():
     if request.method == 'GET':
-        return render_template('Container/contents.html')
+        return render_template('Container/contents.html', path_dict=path_dict)
     else:
         pass
 
 
 @set_bp.route('/<pathname>',  methods=['GET', 'POST'])
 @login_required
-def display_path():
+def display_path(pathname):
     if request.method == 'GET':
-        return render_template('Container/contents.html')
+        path = path_dict[pathname]
+        return render_template('Container/display.html', path=path)
     else:
         pass
 
-
-@set_bp.route('/<pathname>/<filename>',  methods=['GET', 'POST'])
-@login_required
-def display_pic():
-    if request.method == 'GET':
-        return render_template('Container/contents.html')
-    else:
-        pass
